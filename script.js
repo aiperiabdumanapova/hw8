f5bf0561298382d9866b56034239d8f1780547f3
unknown <you@example.com>
2024-04-15 21:21:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,300 @@
+// const data = [{
+//   "title": "orchestrate",
+//   "description": "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 47,
+//   "oldPrice": 9,
+//   "id": "1"
+// },
+// {
+//   "title": "pink",
+//   "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 69,
+//   "oldPrice": 26,
+//   "id": "2"
+// },
+// {
+//   "title": "until",
+//   "description": "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 68,
+//   "oldPrice": 42,
+//   "id": "3"
+// },
+// {
+//   "title": "Touring",
+//   "description": "The Football Is Good For Training And Recreational Purposes",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 88,
+//   "oldPrice": 59,
+//   "id": "4"
+// },
+// {
+//   "title": "demean",
+//   "description": "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 19,
+//   "oldPrice": 93,
+//   "id": "5"
+// },
+// {
+//   "title": "deposit",
+//   "description": "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 78,
+//   "oldPrice": 90,
+//   "id": "6"
+// },
+// {
+//   "title": "withdrawal",
+//   "description": "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 76,
+//   "oldPrice": 53,
+//   "id": "7"
+// },
+// {
+//   "title": "ick",
+//   "description": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 25,
+//   "oldPrice": 15,
+//   "id": "8"
+// },
+// {
+//   "title": "Buckinghamshire",
+//   "description": "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 24,
+//   "oldPrice": 21,
+//   "id": "9"
+// },
+// {
+//   "title": "Azerbaijan",
+//   "description": "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+//   "img": "https://loremflickr.com/640/480/cats",
+//   "price": 0,
+//   "oldPrice": 52,
+//   "id": "10"
+// }
+// ]
+
+const array = [{
+  "title": "Investment",
+  "description": "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 98,
+  "oldPrice": 10,
+  "id": "1"
+},
+{
+  "title": "Unions",
+  "description": "The automobile layout consists of a front-engine design, with transaxle-type transmissions mounted at the rear of the engine and four wheel drive",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 29,
+  "oldPrice": 52,
+  "id": "2"
+},
+{
+  "title": "Hybrid",
+  "description": "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 41,
+  "oldPrice": 7,
+  "id": "3"
+},
+{
+  "title": "interface",
+  "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 42,
+  "oldPrice": 98,
+  "id": "4"
+},
+{
+  "title": "Music",
+  "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 25,
+  "oldPrice": 34,
+  "id": "5"
+},
+{
+  "title": "metrics",
+  "description": "The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 91,
+  "oldPrice": 3,
+  "id": "6"
+},
+{
+  "title": "green",
+  "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 85,
+  "oldPrice": 1,
+  "id": "7"
+},
+{
+  "title": "Folding",
+  "description": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 12,
+  "oldPrice": 70,
+  "id": "8"
+},
+{
+  "title": "Plastic",
+  "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 76,
+  "oldPrice": 43,
+  "id": "9"
+},
+{
+  "title": "West",
+  "description": "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 76,
+  "oldPrice": 28,
+  "id": "10"
+},
+{
+  "title": "Chief",
+  "description": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 74,
+  "oldPrice": 81,
+  "id": "11"
+},
+{
+  "title": "notwithstanding",
+  "description": "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 71,
+  "oldPrice": 21,
+  "id": "12"
+},
+{
+  "title": "policy",
+  "description": "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 59,
+  "oldPrice": 66,
+  "id": "13"
+},
+{
+  "title": "Keyboard",
+  "description": "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+  "img": "https://loremflickr.com/640/480/nature",
+  "price": 86,
+  "oldPrice": 48,
+  "id": "14"
+}
+]
+
+const data = JSON.parse(localStorage.getItem('data')) || []
+
+
+const container = document.getElementById('container')
+const min = document.getElementById('min')
+const max = document.getElementById('max')
+const filterbtn = document.getElementById('filterbtn')
+const search = document.getElementById('search')
+const mask = document.getElementById('mask')
+
+const title = document.getElementById('title')
+const description = document.getElementById('description')
+const img = document.getElementById('img')
+const price = document.getElementById('price')
+const oldPrice = document.getElementById('oldPrice')
+const id = document.getElementById('id')
+
+function deleteProductByName(productName) {
+  var products = JSON.parse(localStorage.getItem('products')) || [];
+  products = products.filter(function(product) {
+      return product.title !== productName;
+  });
+  localStorage.setItem('products', JSON.stringify(products));
+}
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('delete-btn')) {
+      var productCard = event.target.closest('.product-card');
+      var productName = productCard.querySelector('h3').innerText;
+      deleteProductByName(productName);
+      productCard.remove();
+  }
+});
+
+
+function AddNewObj() {
+  const newOBJ = {
+    "title": title.value,
+    "description": description.value,
+    "img": img.value,
+    "price": price.value,
+    "oldPrice": oldPrice.value,
+    "id": id.value
+  } 
+  const fdsfs = [...data, newOBJ]
+  data.unshift(newOBJ)
+  localStorage.setItem('data', JSON.stringify(data))
+  RenderItems(data)
+  Close()
+}
+  
+function Close () {
+mask.style.display = 'none'
+}
+
+function Open() {
+mask.style.display = 'block'
+}
+
+function Search(){
+  console.log(search.value);
+  const newArray = data.filter((element) => 
+      element.title === search.value
+  )
+  console.log(newArray);
+  return RenderItems(newArray)
+  
+}
+
+function Filter(){
+  const newArray = []
+  for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+      if(element.price > +min.value && element.price < +max.value){           
+          newArray.push(element)                       
+      }
+  }
+  return RenderItems(newArray)
+}
+
+function Clear() {
+  RenderItems(data)
+  
+}
+
+function RenderItems(data){
+  container.innerHTML = ''
+  data.forEach((element) =>
+      container.innerHTML += `
+          <div class="card">
+              <img src="${element.img}"alt="">
+              <h3>${element.title}</h3>
+              <p>${element.description}</p>
+              <div>
+              <b>${element.price}</b>
+              <u>${element.oldPrice}</u>
+              </div>
+              <button>add to cart</button>
+          </div>
+  `
+  )
+}
+
+
+RenderItems(array)
